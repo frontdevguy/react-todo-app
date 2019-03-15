f37abbf500f9104bf461ed39979085a989fc8f68
Joshua <ogunbojoshua@gmail.com>
2019-03-15 19:59:27 +0100

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,7 @@ let Todo = ( { todo , index , removeItem, markAsComplete } ) => {
               <span className={ todo.completed ? 'badge completed' : 'badge pending' } > { todo.completed ? 'completed' : 'pending' } </span>
               { !todo.completed && <button className="mark-todo-as-complete" onClick = { () => markAsComplete ( index ) } > <i className="fa fa-check"></i> </button> }
               <button className="remove-todo" onClick = { () => removeItem ( index ) } > <i className="fa fa-trash"></i> </button>
-              <li className="todo-item pending">
+              <li className="todo-item">
                     <p className="todo-title"> { todo.title } </p>
               </li>
          </div>
